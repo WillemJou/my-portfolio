994f67b35fe4aacbaa39d2923ab92ae1a9432fa8
Willem Jouret <willem.jouret9@gmail.com>
2024-03-16 22:49:59 +0100

    Change box style and tag
    - Animate on techno icons when click on the parent element
    - Change height of the fade-stack element and text-align inside it
    -change button tag to div with tabIndex (a div inside a button is invalid HTML)

--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -19,7 +19,8 @@ export function Stack({ stack }: StackProps) {
   }
 
   return (
-    <button
+    <div
+      tabIndex={0}
       key={stack?.id}
       className='stack stack-element pointer relative gap-2'
       onClick={() => handleOpen()}>
@@ -48,12 +49,17 @@ export function Stack({ stack }: StackProps) {
         className={`${
           isMounted ? 'techno-icon_opened' : 'techno-icon_closed'
         }`}>
-        <img
+        <div
           className={`${
-            stack?.title === 'JavaScript' ? 'js-anim-icon' : 'react-anim-icon'
-          }`}
-          src={stack?.icon}
-        />
+            stack?.title === 'JavaScript' ? 'bouncing-div' : 'rotate-div'
+          }`}>
+          <img
+            className={`${
+              stack?.title === 'JavaScript' ? 'js-anim-icon' : 'react-anim-icon'
+            }`}
+            src={stack?.icon}
+          />
+        </div>
       </div>
       <svg
         className={
@@ -65,6 +71,6 @@ export function Stack({ stack }: StackProps) {
         viewBox='0 0 1024 1024'>
         <path d='m488.832 344.32l-339.84 356.672a32 32 0 0 0 0 44.16l.384.384a29.44 29.44 0 0 0 42.688 0l320-335.872l319.872 335.872a29.44 29.44 0 0 0 42.688 0l.384-.384a32 32 0 0 0 0-44.16L535.168 344.32a32 32 0 0 0-46.336 0'></path>
       </svg>
-    </button>
+    </div>
   )
 }
