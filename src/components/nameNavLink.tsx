735ccde33dc3ecff2673c8d597c02c76d3fa9ead
Willem Jouret <willem.jouret9@gmail.com>
2025-07-23 16:20:19 +0200

    Add dynamic redirect depend on location

--- src/components/nameNavLink.tsx
+++ src/components/nameNavLink.tsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 export function NameNavLink() {
+  const urlLocation = useLocation()
+  const dynamicRedirect = urlLocation.pathname.includes('about')
+    ? '/my-portfolio/'
+    : '/my-portfolio/about'
+
   return (
     <>
       <div className="simple-row-layout">
-        <Link to="/my-portfolio/about" className="me-link lg-font-size">
+        <Link to={dynamicRedirect} className="me-link lg-font-size">
           Willem Jouret
         </Link>
       </div>
